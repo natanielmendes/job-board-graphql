2ef08f995e043431203b0b757d2ee833fa2f633b
Nataniel Mendes <natanielmendes@hotmail.com>
2022-11-20 20:40:58 +0000

    Job-Company Association

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,13 @@
-import { Job } from './db.js'
+import { Company, Job } from './db.js'
 
 export const resolvers = {
   Query: {
     jobs: () => Job.findAll()
   },
+
+  Job: {
+    company: (job) => {
+      return Company.findById(job.companyId)
+    }
+  }
 }
