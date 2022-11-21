39a9f69c8286a7bc81f5431cd0ac1ebcdfe7e0ed
Nataniel Mendes <natanielmendes@hotmail.com>
2022-11-21 13:02:40 +0000

    GraphQL-Request Client

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,15 +1,16 @@
-import JobList from './JobList';
-import { jobs } from '../fake-data';
+import JobList from './JobList'
+import { jobs } from '../fake-data'
+import { getJobs } from '../graphql/queries'
+
+getJobs()
 
 function JobBoard() {
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
-  );
+  )
 }
 
-export default JobBoard;
+export default JobBoard
